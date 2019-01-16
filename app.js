86f4cfd850f89bdae76a6668a00c2df156bb0fbf
Shivang Ranjan <shivang12134@gmail.com>
2019-01-16 16:19:11 +0530

    Selecting elements with the same class name

--- app.js
+++ app.js
@@ -11,13 +11,23 @@
 
 const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('MyTextInput');
+const myTextInput = document.getElementById('myTextInput');
 
 
 myButton.addEventListener('click', () => {
   myHeading.style.color = myTextInput.value;
 });
 
-//key pressers, scrolling and mouse moves.
 
 
+const myList = document.getElementsByTagName('li')
+for(let i = 0; i <myList.length; i +=1) {
+  myList[i].style.color = 'purple';
+}
+
+
+const myList = document.getElementsByClassName('error-not-purple');
+
+for(let i = 0; i<errorNotPurple.length; i += 1) {
+  errorNotPurple[i].style.color = 'red';
+}
