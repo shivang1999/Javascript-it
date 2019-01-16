cd54539133290446d94e660ffa7a96288d3b3800
Shivang Ranjan <shivang12134@gmail.com>
2019-01-16 18:38:04 +0530

    Appending Nodes

--- Creating New DOM elements/NewDOM.js
+++ Creating New DOM elements/NewDOM.js
@@ -23,11 +23,25 @@ toggleList.addEventListener('click', () => {
  
 descriptionbutton.addEventListener('click', () => {
   descriptionp.innerHTML = descriptioninput.value + ':';
+  descriptioninput.value = '';
 });
 
 addItemButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
     li.textContent = addItemInput.value ;
+    ul.appendChild(li);
+    addItemInput.value = '';
 });
 
+//AddItem button is the listener which is helping it create all the tags button.
+
+//Task to do-
+
+//Create a contact us page using this
+
+
 //naming variables well
+
+
+//Node.appendChild method is used to append it.
