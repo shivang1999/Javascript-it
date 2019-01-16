3eaa0b1d7f40e2bb86bc3627840c522cbbe822bd
Shivang Ranjan <shivang12134@gmail.com>
2019-01-16 09:53:13 +0530

    Learning Arrow Functions

---
+++ arrow.js
@@ -0,0 +1,61 @@
+const name = "Andrew";
+function sayName() {
+  const message = "My name is" + name;
+  console.log(message);
+}
+function sayBye() {
+  console.log("Bye" + name);
+}
+
+const name = "Shivang Ranjan";
+const sayName = () => {
+  const message = "My name is" + name;
+  console.log(message)
+}
+
+const square = (x) => {
+  return x*x;
+}
+const cube = (x) => {
+  return square(x) * x;
+}
+
+const add = (a,b) => {
+  return a+b;
+}
+const sub = (w,x) => {
+  return w-x;
+}
+const multiply = (s,r) => {
+  return s*r;
+}
+const name = "Shivang";
+const sayName = () => {
+  const message = "My name is" + name;
+  console.log(message);
+}
+
+const sayBye = () => {
+  console.log("Bye" + name;
+}
+
+//Function Declaration 
+function divide1(a.b) {
+  return a/b;
+}
+
+//Function Expression 
+const divide2 = function(a.b) {
+  return a/b;
+}
+
+//Arrow Function Expession
+const divide3 = (a,b) => {
+  return a/b;
+}
+
+//Arrow Function-consise
+
+const divide4 = (a,b) => a/b ;
+
+
