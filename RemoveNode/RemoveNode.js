9d58439ef48b3f9aff6b8016271a7a631b4ed71b
Shivang Ranjan <shivang12134@gmail.com>
2019-01-18 18:08:20 +0530

    addEventListener() modified

--- RemoveNode/RemoveNode.js
+++ RemoveNode/RemoveNode.js
@@ -6,17 +6,21 @@ const descriptionbutton = document.querySelector('button');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
-const listItems = document.getElementsByTagName('li')[0];
+const listItems = document.getElementsByTagName('li');
+
+for (let i = 0; i< listItems.length ; i += 1) {
+    listItems[i].addEventListener('mouseover', ()=> {
+      listItems[i].textContent = listItems[i].textContent.toUpperCase();
+    });
+    
+    listItems[i].addEventListener('mouseout', ()=> {
+      listItems[i].textContent = listItems[i ].textContent.toLowerCase();
+    });
+    
+}
 
 
 
-listItems.addEventListener('mouseover', ()=> {
-  listItems.textContent = listItems.textContent.toUpperCase();
-});
-
-listItems.addEventListener('mouseout', ()=> {
-  listItems.textContent = listItems.textContent.toLowerCase();
-});
 
 
 toggleList.addEventListener('click', () => {
