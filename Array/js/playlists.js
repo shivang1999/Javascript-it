86502283b445972cc4ecd4f510c406640c00536e
Shivang Ranjan <shivang12134@gmail.com>
2019-01-18 21:09:29 +0530

    Accessing Arrays

---
+++ Array/js/playlists.js
@@ -0,0 +1,11 @@
+var playList = [];
+playList.push('I Did It My Way');
+playList.push('I Want It That Way');
+playList.push('Wrecking Ball', 'Lightening');
+playList.unshift('Born To Run');
+playList.unshift('')
+
+
+
+printList(playList);
+
